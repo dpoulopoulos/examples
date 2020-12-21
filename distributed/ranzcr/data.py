2b46841f3c2f1adf4f3aed9f0699cc0a9e06fba9
Dimitris Poulopoulos <dimpo@arrikto.com>
2020-12-21 15:18:30 +0000

    distributed: RANZCR distributed

--- distributed/ranzcr/data.py
+++ distributed/ranzcr/data.py
@@ -1,42 +1,53 @@
+from pathlib import Path
+
+import pandas as pd
 import torch
 import torchvision.transforms as transforms
 
 from PIL import Image
 from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.distributed import DistributedSampler
+
+
+TRAIN_PATH  = Path("data/train")
+DATA_PATH   = Path("data/train.csv")
+TARGET_COLS = ['ETT - Abnormal', 'ETT - Borderline', 'ETT - Normal',
+               'NGT - Abnormal', 'NGT - Borderline', 'NGT - Incompletely Imaged', 'NGT - Normal', 
+               'CVC - Abnormal', 'CVC - Borderline', 'CVC - Normal',
+               'Swan Ganz Catheter Present']
 
 
 class RanzcrDataset(Dataset):
-    def __init__(self, df, target_cols, path, tfms=None):
-        self.path = path
-        self.df = df
-        self.file_names = df['StudyInstanceUID'].values
-        self.labels = df[target_cols].values
+    def __init__(self, tfms=None):
+        self.df = pd.read_csv(DATA_PATH)
+        self.file_names = self.df['StudyInstanceUID'].values
+        self.labels = self.df[TARGET_COLS].values
         self.tfms = tfms
         
     def __len__(self):
         return len(self.df)
 
-    def __getitem__(self, idx):
-        file_name = self.file_names[idx]
-        file_path = f'{self.path}/{file_name}.jpg'
+    def __getitem__(self, i):
+        file_name = self.file_names[i]
+        file_path = f"{TRAIN_PATH}/{file_name}.jpg"
         image = Image.open(file_path)
         if self.tfms:
             augmented = self.tfms(image)
             image = augmented
-        label = torch.tensor(self.labels[idx]).float()
+        label = torch.tensor(self.labels[i]).float()
         return image, label
     
 
-def get_data(folds, target_cols, path, batch_size: int = 128, 
-             shuffle: bool = True, drop_last: bool = True):
+def get_data(args, rank):
     _transforms = transforms.Compose([transforms.RandomResizedCrop(512),
                                       transforms.ToTensor(),
                                       transforms.Normalize((0.1307,), (0.3081,))
                                      ])
-    dataset = RanzcrDataset(folds, target_cols, path, _transforms)
-    dataloader = DataLoader(dataset, batch_size=batch_size,
-                            shuffle=shuffle, num_workers=0,
-                            pin_memory=True, drop_last=drop_last)
+    dataset = RanzcrDataset(_transforms)
+    sampler = DistributedSampler(dataset, num_replicas=args.world_size,
+                                 rank=rank)
+    dataloader = DataLoader(dataset, batch_size=args.batch_size,
+                            shuffle=False, num_workers=0,
+                            pin_memory=True, sampler=sampler)
     return dataloader
-    
     
